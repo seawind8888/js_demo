bf8c11e68f7945acc3434e953fb93a3f9644162b
seawind8888 <344023858@qq.com>
2018-02-26 19:45:36 +0800

    modify

--- async-await.js
+++ async-await.js
@@ -1,6 +1,16 @@
-async function test(a=0) {
-  await console.log(a)
-  a++
-  await console.log(a)
+function fetch(count) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(count)
+    }, 1000)
+  })
 }
+
+
+async function test() {
+  for (let i = 0; i < 5; i++) {
+    console.log(await fetch(i))
+  }
+}
+
 test()
