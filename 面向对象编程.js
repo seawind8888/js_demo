bc49aac80205a3c558b2e0e0b337ac291ddd75b7
harvest_wang <344023858@qq.com>
2018-12-24 14:38:50 +0800

    commit

--- 面向对象编程.js
+++ 面向对象编程.js
@@ -1,15 +1,24 @@
-继承形式
-function Person () {}
-Person.prototype.Talk = (mes) => {'say:' + console.log(mes)}
-Person.prototype.Eat = (food) => {'eat' + console.log(food)}
+// 继承形式
+
+function Person() {}
+Person.prototype.Talk = (mes) => {
+    'say:' + console.log(mes)
+}
+Person.prototype.Eat = (food) => {
+    'eat' + console.log(food)
+}
 let man = new Person()
 man.Talk('hahah')
 man.Eat('apple')
 
 // 字面量形式
 let Person = {
-    Talk: (mes) => {'say:' + console.log(mes)},
-    Eat: (food) => {'eat' + console.log(food)}
+    Talk: (mes) => {
+        'say:' + console.log(mes)
+    },
+    Eat: (food) => {
+        'eat' + console.log(food)
+    }
 }
 let man = Person
 man.Talk('hahah')
