3c8743d89706d8e065803b6bfc8fc141f7ce87f5
harvest_wang <344023858@qq.com>
2019-02-12 16:11:50 +0800

    commit

--- nodejs/async-mongoose-test.js
+++ nodejs/async-mongoose-test.js
@@ -3,20 +3,34 @@ const asyncMongo = require('./async-mongoose.js')
 mongoose.connect('mongodb://localhost:27017/test',{useNewUrlParser: true});
 
 const TestModule = mongoose.model('Test', new mongoose.Schema({
-    name: String
+    name: String,
+    list: [
+        {
+            create_at: String,
+            update_at: [{aaa: String}]
+        }
+    ]
 }));
 async function start() {
     try {
         await asyncMongo({
             model: TestModule,
             method: 'create',
-            key: {name: 111}
+            key: {name: "111"}
         })
-        await asyncMongo({
-            model: TestModule,
-            method: 'updateOne',
-            key: {name: 222}
-        })
+        // await asyncMongo({
+        //     model: TestModule,
+        //     method: 'updateOne',
+        //     key: {name: 222}
+        // })
+        // let doc = await asyncMongo({
+        //     model: TestModule,
+        //     method: 'updateOne',
+        //     key:  {name: "111"},
+        //     options: {upsert: true},
+        //     value: {list: [{create_at: '222', update_at: []}]}
+        // })
+        //   console.log(doc)
         
     } catch (error) {
         
