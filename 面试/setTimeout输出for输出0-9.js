2e7a2f02fd34c90a7172f773d895c7aa4831e555
seawind8888 <344023858@qq.com>
2020-01-06 11:42:53 +0800

    commit1
    commit2
    
    commit1
    
    commit2

--- 面试/setTimeout输出for输出0-9.js
+++ 面试/setTimeout输出for输出0-9.js
@@ -5,11 +5,11 @@ for (var i = 0; i< 10; i++){
     }, 1000,i)
  }
 
-//  // 解法2
-//  for (var i = 0; i< 10; i++){
-//     ((i) => {
-//       setTimeout(() => {
-//         console.log(i);
-//       }, 1000)
-//    })(i)
-//   }
+ // 解法2
+ for (var i = 0; i< 10; i++){
+    ((i) => {
+      setTimeout(() => {
+        console.log(i);
+      }, 1000)
+   })(i)
+  }
